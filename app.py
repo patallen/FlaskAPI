be7b7a8bd8d73da38ef0c50aab403868cb86f46a
Patrick Allen <prallen90@gmail.com>
2015-12-25 13:57:02 -0500

    Add password functionality to User model

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///polls.db'
 api = Api(app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
--- models/users.py
+++ models/users.py
@@ -1,4 +1,5 @@
 from app import db
+from app import bcrypt
 
 
 class User(db.Model):
@@ -9,3 +10,16 @@ class User(db.Model):
     last_name = db.Column(db.String(64))
     signup_date = db.Column(db.DateTime(), default=db.func.now())
     last_login = db.Column(db.DateTime(), default=db.func.now())
+
+    _password_hash = db.Column(db.String())
+
+    @property
+    def password(self):
+        return self._password_hash
+
+    @password.setter
+    def password(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password)
+
+    def verify_password(self, password_hash):
+        return bcrypt.check_password_hash(self.password, password_hash)
