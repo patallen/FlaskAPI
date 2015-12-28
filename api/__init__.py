a4f352f460bf87f13b6e7b383378ece58d46cddf
Patrick Allen <prallen90@gmail.com>
2015-12-28 02:16:39 -0500

    Add helper function to get JWT from request

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,10 @@
 from app import app
 from models.users import User
-from itsdangerous import JSONWebSignatureSerializer
+from itsdangerous import JSONWebSignatureSerializer, BadSignature
 from flask import request, Response
 from decorators import crossdomain
+import json
 
-import pprint
 
 jss = JSONWebSignatureSerializer('dude')
 
@@ -12,7 +12,6 @@ jss = JSONWebSignatureSerializer('dude')
 @app.route('/authenticate/', methods=['POST'])
 @crossdomain(origin="*")
 def authenticate_for_jwt():
-    pprint.pprint(request.headers)
     username = request.form.get('username')
     password = request.form.get('password')
 
@@ -26,3 +25,21 @@ def authenticate_for_jwt():
         token = jss.dumps({'username': username})
         return Response(token, 200)
     return Response("Could not authenticate user.", 400)
+
+
+@app.route('/test/', methods=['POST'])
+def test_jwt():
+    payload = get_jwt_payload()
+    if payload is None:
+        return Response("Trouble Authenticating", 400)
+    return Response(json.dumps(payload), 200)
+
+
+def get_jwt_payload():
+    token_str = request.headers['Authentication']
+    try:
+        token = token_str.split(' ')[1]
+        payload = jss.loads(token)
+    except BadSignature:
+        return None
+    return payload
